3159ce6a0d670c876558e7ea84ac0e24585d4518
Bruno Martins <martins_bruno01@hotmail.com>
2024-11-12 21:57:04 -0300

    Criacao do projeto

---
+++ src/main/java/com/devsuperior/dslistgame/DslistgameApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dslistgame/DslistgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
